12781926d9c52dbf75ed059107a93da741ff5048
Pooja Sharma <poojasharma5501@gmail.com>
2019-08-28 15:50:01 +0530

    first commit

---
+++ 1st.php
@@ -0,0 +1,133 @@
+<?php 
+// $a='b';
+// if (($a=='a') || ($a=='e') || ($a=='i') ||($a=='o') || ($a=='u')) {
+// 	echo "vowel";
+// }
+// else{
+// 	echo "<center>error<br>";
+// }
+
+// # if else
+
+// $a=array("abh","dsar","hjgs","hgsh","ghhg");
+// if (in_array("dsar", $a)) {
+// 	echo "<h1>found</h1>";
+// }
+// else{
+// 	echo "not found";
+// }
+//  $b=count($a);
+
+
+//  if ($a>11) {
+//  	echo "ok";
+//  }
+//  else{
+//  	echo "not ok";
+//  }
+
+//  $num1 = 12;
+
+//  if ($num1%2==0) {
+//  	echo "<br>even";
+//  }
+//  else{
+//  	echo "<br>odd";
+//  }
+//  # WHILE LOOPS
+
+// $x=3;
+// $y=1;
+// while ($x<= 30) {
+// 	echo "<br>3 x ".$y."= ".$x."<br>";
+// 	$x=$x+3;
+// 	$y++;
+// }
+
+// $k=4;
+// $l=1;
+// while ($k<= 40) {
+// 	echo "<br>4 x ".$l."= ".$k."<br>";
+// 	$k=$k+4;
+// 	$l++;
+// }
+// $m=5;
+// $n=1;
+// while ($m<= 50) {
+// 	echo "<br>5 x ".$n."= ".$m."<br>";
+// $m=$m+5;
+// $n++;
+// }
+
+// $p=6;
+// $q=1;
+// while ($p<= 60) {
+// 	echo "<br>6 x ".$q."= ".$p."<br>";
+// $p=$p+6;
+// $q++;
+// }
+// $w=19;
+// $j=1;
+// while ($w<= 190) {
+// 	echo "<br>19 x ".$j."= ".$w."<br>";
+// 	$w=$w+19;
+// 	$j++;
+// }
+
+
+// $v=0;
+// while ($v<=99) {
+// 	$v++;
+// 	if ($v%2==0) {
+
+// 	}
+// 	else{
+// 		echo $v."<br>";
+
+// 	}
+// }
+
+// # FOR LOOPS
+
+// for ($i=2; $i <=100; $i=$i+2) { 
+// 	  echo "$i<br>";
+// }
+
+// for ($i=2; $i<=20 ; $i=$i+2) { 
+// 	echo "<b>$i</b><br>";
+// }
+
+// for ($i=15; $i <=150 ; $i=$i+15) { 
+// 	echo "<b>$i</b><br>";
+// }
+
+
+// for ($i=2; $i <52; $i=$i+2) { 
+// 	echo "$i<br>";
+// }
+// for ($i=19; $i >=8 ; $i--) { 
+// 	echo "<b>$i</b><br>";
+// }
+
+
+// $i=1;
+// while ( $i<=12) {
+// 	echo "*";
+// 	$i++;
+// 	if ($i==5 || $i==9 ) {
+// 		echo "<br>";
+		
+// 	}
+// }
+
+
+$i=1;
+while ( $i<=10) {
+	echo "*";
+	$i++;
+	if ($i==2 || $i==4 || $i==7 ) {
+		echo "<br>";
+	}
+}
+
+?>
